76bc5c93c2a7565dd478a9355a23284a54c1f9f3
ljorente <ljorente@fattail.com>
2025-06-24 11:50:28 -0300

    network intercept test

---
+++ cypress/e2e/UI-tests/network-intercept-tests.spec.cy.js
@@ -0,0 +1,42 @@
+import '../../support/commands/ui-commands/login-commands.js'
+import '../../support/commands/ui-commands/component-commands/app-header-component-commands.js'
+import locators from '../../support/locators.js'
+
+let toastmessagedata
+let topMenuComponentData
+const baseUrlWeb = Cypress.env('BASE_URL_WEB')
+const userName = Cypress.env('USER')
+const password = Cypress.env('PASSWORD')
+
+describe('E2E Login Tests', () => {
+  before(() => {
+    //captures the json data for the tests
+    cy.fixture('label-messages/top-menu-component').then((data) => {
+      topMenuComponentData = data
+    })
+    cy.fixture('label-messages/toast-message').then((data) => {
+      toastmessagedata = data
+    })
+  })
+
+  beforeEach(() => {
+    cy.visit(baseUrlWeb)
+
+  })
+
+  it('LoginTC01: LOG test', { tags: '@smoke' }, () => {
+    cy.intercept('POST', '/signin').as('submitRequest')
+
+    cy.SelectSettingsOption(topMenuComponentData.settings_option_logout)
+    cy.get(locators.TOAST_MESSAGE, { timeout: 10000 }).should('contain',toastmessagedata.successLogout)
+
+    cy.InsertEmail(userName)
+    cy.InsertPassword(password)
+    cy.ClickLoginButton()
+    cy.wait('@submitRequest').then((interception) => {
+      expect(interception.request.method).to.equal('POST')
+      expect(interception.response.statusCode).to.equal(200)
+      expect(interception.response.body).to.have.property('nome', "leonardo Jorente")
+    })
+  })
+})
