ca2ec1343e9edcfaa2fec111204f9cd1eded17c5
ljorente <ljorente@fattail.com>
2025-07-21 14:03:20 -0300

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require("cypress")
-const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
-
 
 const dotEnvPath = process.env.TEST_ENV ? `.env.${process.env.TEST_ENV}` : '.env'
 require('dotenv').config({
@@ -9,37 +7,27 @@ require('dotenv').config({
 })
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
   e2e: {
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'Cypress Test Report',
-      reportDir: 'cypress/mochawesome-reports',
-      overwrite: true,
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false,
-      autoOpen: false,
-      html: true,
-      json: false,
-    },
+  setupNodeEvents(on, config) {
+    require('@cypress/grep/src/plugin')(config); // Register the grep plugin for filtering tests
+    require('cypress-mochawesome-reporter/plugin')(on);
 
-    setupNodeEvents(on, config) {
-      require('@cypress/grep/src/plugin')(config); // Register the grep plugin for filtering tests
-      require('cypress-mochawesome-reporter/plugin')(on);
+    config.env = {// Load environment variables from .env file
+      ...process.env
+    }
 
-      config.env = {// Load environment variables from .env file
-        ...process.env
+    on('task', {// Define a task to log messages in Cypress
+      log(message) {
+        console.log('Cypress log:', message);
+        return null; // Must return something (or `null`)
       }
+    })
 
-      on('task', {// Define a task to log messages in Cypress
-        log(message) {
-          console.log('Cypress log:', message);
-          return null; // Must return something (or `null`)
-        }
-      })
-
-      return config;
+    return config;
     },
   },
 });
